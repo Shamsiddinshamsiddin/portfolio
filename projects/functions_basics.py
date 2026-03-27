8cebdbc1752d8110b669cfb7235358de10210449
shamsiddinshamsiddin <akangkuchaydi312@gmail.com>
2026-03-27 21:10:23 +0500

    def munction mustahkamlandi

---
+++ projects/functions_basics.py
@@ -0,0 +1,5 @@
+#I learned Python functions. - def keyword- arguments (name, age)- calling functions
+def my_info(name, age):
+    print("My name is" + name)
+    print("I'm" + age + "years old")
+my_info("Shamsiddin", "16")
