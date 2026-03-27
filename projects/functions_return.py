ed6bbb1bad113f71aa05c71107ae096e32428383
shamsiddinshamsiddin <akangkuchaydi312@gmail.com>
2026-03-27 21:20:10 +0500

    return organildi

---
+++ projects/functions_return.py
@@ -0,0 +1,10 @@
+#I learned return in Python.
+#-return gives result back 
+#-functions can calculate values
+
+def multiply(a, b):
+    return a * b
+
+print(multiply(3, 4))
+
+#javob 12 - chunki bu yerda kopaytirish bajarildi
