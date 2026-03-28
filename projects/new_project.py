b475d4e5d7e7bef5c1fc4cf28369d63a9cc4c824
shamsiddinshamsiddin <akangkuchaydi312@gmail.com>
2026-03-28 21:51:40 +0500

    Added calculator v1

---
+++ projects/new_project.py
@@ -0,0 +1,4 @@
+#Maximum va minumimini topish
+numbers = [2,4,6,8,1,3,5,7,9,10]
+print("max: ", max(numbers))
+print("min: ", min(numbers))
---
+++ projects/new_pythonfunction_project.py
@@ -0,0 +1,5 @@
+#Ko'paytirish amalini o'rganamiz:
+def kopaytirish(a,b,c):
+    return a * b * c
+result = kopaytirish(9, 18, 27)
+print(result)
